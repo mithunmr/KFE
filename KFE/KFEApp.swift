b71f8a911d6b360c641b59d5b520b55a9f7ecc7d
MITHUN M R <mithunmr286@gmail.com>
2023-12-13 19:29:16 +0530

    Initial Commit

---
+++ KFE/KFEApp.swift
@@ -0,0 +1,17 @@
+//
+//  KFEApp.swift
+//  KFE
+//
+//  Created by MRM on 13/12/23.
+//
+
+import SwiftUI
+
+@main
+struct KFEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
